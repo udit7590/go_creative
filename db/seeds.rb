d9dc88c0ac63ed3860d876f32a43517110a828b6
uditmittal <udit@vinsol.com>
2014-12-09 18:22:40 +0530

    CF-1 Removes Comments from migration files. Adds seeds.rb file https://vinsol.atlassian.net/browse/CF-1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Admin.create!(email: 'udit@vinsol.com', password: 'admin123', password_confirmation: 'admin123')
