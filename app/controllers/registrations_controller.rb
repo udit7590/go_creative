f4b4946454f747524527b9c00ce0dfd860fee1b1
uditmittal <udit@vinsol.com>
2014-12-12 19:27:55 +0530

    CF4, CF5 Adds User Login and Registration Functionality https://vinsol.atlassian.net/browse/CF-4 https://vinsol.atlassian.net/browse/CF-5

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,18 +16,17 @@ class RegistrationsController < Devise::RegistrationsController
     build_resource(sign_up_params)
     resource_saved = resource.save
     yield resource if block_given?
+    @user = resource
     if resource_saved
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up
         sign_up(resource_name, resource)
-        # respond_with resource, location: after_sign_up_path_for(resource)
         respond_to do |format|
           format.js { render 'registrations/success_registration' }
         end
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
-        # respond_with resource, location: after_inactive_sign_up_path_for(resource)
         respond_to do |format|
           format.js { render 'registrations/success_registration' }
         end
@@ -38,7 +37,6 @@ class RegistrationsController < Devise::RegistrationsController
       if @validatable
         @minimum_password_length = resource_class.password_length.min
       end
-      # respond_with resource
       respond_to do |format|
         format.js { render 'registrations/error_registration' }
       end
