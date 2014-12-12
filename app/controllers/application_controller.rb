43985cd83876b41a89ced2ba485377bd994dbbc0
uditmittal <udit@vinsol.com>
2014-12-12 14:12:02 +0530

    CF4,CF5 Adds user registration functionality https://vinsol.atlassian.net/browse/CF-4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  #For permitting additional attributes to users devise model
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :country, :state, :city, :pincode, :address_line_1, :address_line_2, :phone_number) }
+  end
 end
