4f33562ecd0ea69048f782e30380787db8a49a32
uditmittal <udit@vinsol.com>
2014-12-15 17:08:02 +0530

    Fixes configuration for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Gocreative::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    password: ENV['SENDGRID_PASSWORD'], 
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
