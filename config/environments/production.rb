bdeeb5bb51c04a7f5f03da27559d2f7bebb379cf
uditmittal <udit@vinsol.com>
2014-12-15 12:40:34 +0530

    CF3 Fixes error message on invalid admin login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,14 @@ Gocreative::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   config.action_mailer.default_options = { from: 'site@gocreative.com' }
 end
