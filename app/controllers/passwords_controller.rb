7dc63bd253706e3433c4becff110d1e619cc8e41
uditmittal <udit@vinsol.com>
2014-12-16 13:05:22 +0530

    CF6 Fix- Checks if user is confirmed before we resets the password https://vinsol.atlassian.net/browse/CF-6

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,5 +1,7 @@
 class PasswordsController < ::Devise::PasswordsController
 
+  before_action :check_user_confirmed, only: :create
+
   def after_resetting_password_path_for(resource)
     root_path
   end
@@ -7,5 +9,13 @@ class PasswordsController < ::Devise::PasswordsController
   def after_sending_reset_password_instructions_path_for(resource)
     root_path
   end
+
+  protected
+
+    def check_user_confirmed
+      user = User.find_by_email(params[:user][:email])
+      set_flash_message :notice, :reset_password_needs_confirmation
+      redirect_to new_confirmation_path(:user) unless user && user.confirmed?
+    end
 
 end
