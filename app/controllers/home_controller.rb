30fa7578650b754ddbe4acae7ae24dbced1a22f6
uditmittal <udit@vinsol.com>
2014-12-31 19:29:12 +0530

    S3 Adds Home page functionality https://vinsol.atlassian.net/browse/CF-21

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
       @projects = YAML.load(Rails.cache.read('our_best_projects'))
     else
       @projects = Project.best_projects
-      Rails.cache.write('our_best_projects', @projects.to_yaml, expires_in: 2.minutes)
+      Rails.cache.write('our_best_projects', @projects.to_yaml, expires_in: 30.minutes)
     end
   end
 
