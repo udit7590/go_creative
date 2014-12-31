409414338e4459f9b61222e57b7564a6027e2f75
uditmittal <udit@vinsol.com>
2014-12-31 19:29:12 +0530

    S3 Adds Home page functionality https://vinsol.atlassian.net/browse/CF-21

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  
+  def index
+    @projects = Rails.cache.fetch('our_best_products', expires_in: 30.minutes) do
+      Project.best_projects
+    end
+  end
+
 end
---
+++ app/sweeper/best_projects_sweeper.rb
@@ -0,0 +1,10 @@
+class BestProjectsSweeper < ActionController::Caching::BestProjectsSweeper
+  observe Project
+
+  # Currently this expire is not called from anywhere as we have kept the window for cache for 30 minutes
+  # In case we do want to expire the cache, we can call this method
+  def expire_cache
+    expire_fragment 'our_best_products'
+  end
+
+end
